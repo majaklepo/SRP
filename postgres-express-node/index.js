b846acc43b0a164fe60e5275009b6161f9297750
majaklepo <maja.klepo@hotmail.com>
2021-01-20 01:27:12 +0100

    Initial testing of CASL authorization lib

--- postgres-express-node/index.js
+++ postgres-express-node/index.js
@@ -4,6 +4,7 @@ const {
   ForbiddenError,
   subject,
 } = require("@casl/ability");
+const { underscoredIf } = require("sequelize/types/lib/utils");
 
 // * =============================
 // *  Users
@@ -62,9 +63,35 @@ const MedicalTest_5 = subject("MedicalTest", {
 // * =============================
 // *  Roles <--> permissions
 // * -----------------------------
-function defineAdminRules({ can }, user) {}
+function defineAdminRules({ can }, user) {
+  can("manage","all");
+}
+
 function defineDoctorRules({ can }, user) {}
-function defineUserRules({ can }, user) {}
+
+function defineUserRules({ can }, user) {
+  // can("create","MedicalTest", {UserId: user.id});
+  // can("read","MedicalTest", {UserId: user.id});
+  // can("update","MedicalTest", {UserId: user.id});
+  // can("delete","MedicalTest", {UserId: user.id});
+
+  can(["create","read","update","delete"],
+  "User",
+  ["username", "password"], 
+  {
+    id: user.id
+  }
+  );
+
+  can(
+    ["create","read","update","delete"],
+    "MedicalTest",
+    {
+      UserId: user.id
+    }
+    );
+}
+
 function defineAnonymousRules({ can }, user) {}
 
 // * =============================
